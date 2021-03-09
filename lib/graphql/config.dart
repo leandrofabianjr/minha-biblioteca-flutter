a6397d0b902861982b6859a74ad0c4c8072d6ba6
leandrofabianjr <leandrofabianjr@gmail.com>
2021-03-09 10:52:57 -0300

    Corrigido configuração inicial do graphql

--- lib/graphql/config.dart
+++ lib/graphql/config.dart
@@ -2,23 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class Config {
-  aa() async {
-    await initHiveForFlutter();
-  }
+  static const _END_POINT = 'minha-biblioteca-hasura.herokuapp.com/v1/graphql';
 
-  static final HttpLink httpLink =
-      HttpLink('https://minha-biblioteca-hasura.herokuapp.com/v1/graphql');
+  static String? _token;
 
-  static String _token;
+  static final HttpLink httpLink = HttpLink('https://$_END_POINT');
 
   static final AuthLink authLink = AuthLink(getToken: () => _token);
 
-  final websocketLink = WebSocketLink(
-    url: 'wss://minha-biblioteca-hasura.herokuapp.com/v1/graphql',
+  static final websocketLink = WebSocketLink(
+    'wss://$_END_POINT',
     config: SocketClientConfig(
       autoReconnect: true,
       inactivityTimeout: Duration(seconds: 30),
-      initPayload: () async {
+      initialPayload: () async {
         return {
           'headers': {'Authorization': _token},
         };
@@ -28,8 +25,12 @@ class Config {
 
   static final Link link = authLink.concat(httpLink).concat(websocketLink);
 
-  static ValueNotifier<GraphQLClient> initailizeClient(String token) {
+  static Future<ValueNotifier<GraphQLClient>> initailizeClient(
+    String token,
+  ) async {
     _token = token;
+
+    await initHiveForFlutter();
 
     ValueNotifier<GraphQLClient> client = ValueNotifier(
       GraphQLClient(
